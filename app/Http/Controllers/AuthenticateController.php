48f3aae02ec194cc56150c0ad7ce8a2fbe590885
NAD <lua.solution>
2022-03-14 23:34:00 +0700

    z

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -24,7 +24,7 @@ class AuthenticateController extends Controller
     public function authenticate(Request $request)
     {
         // grab credentials from the request
-        $credentials = $request->only('email', 'password');
+        $credentials = $request->only('phone', 'password');
 
         try {
             // attempt to verify the credentials and create a token for the user
