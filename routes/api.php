31bdfe03aa63cfcc731a3fac9225cf7f55a8fe50
NAD <lua.solution>
2022-05-03 02:21:10 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,6 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
-    Route::get('/test-fcm', function ($user_id) {
-        $res = User::find($user_id);
-        if($res->fcm_token) {
-            send_fcm($res->fcm_token, 'Đăng nhập thành công test FCM', 'Bạn đã đăng nhập vào hệ thống', 'data');
-        }
-
-        return true;
-    });
 });
 
 Route::get('/cmnd_front/{user_id}', function ($user_id) {
@@ -26,4 +17,13 @@ Route::get('/cmnd_front/{user_id}', function ($user_id) {
 
 Route::get('/cmnd_back/{user_id}', function ($user_id) {
     return Image::make(storage_path("app/users/$user_id/cmnd_back.webp"))->response();
+});
+
+Route::get('/test-fcm', function ($user_id) {
+    $res = User::find($user_id);
+    if($res->fcm_token) {
+        send_fcm($res->fcm_token, 'Đăng nhập thành công test FCM', 'Bạn đã đăng nhập vào hệ thống', 'data');
+    }
+
+    return true;
 });
