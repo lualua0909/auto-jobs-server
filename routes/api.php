93e64c460330dea251dca36e64bf5ad5206ae764
NAD <lua.solution>
2022-05-03 02:23:46 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::get('/cmnd_back/{user_id}', function ($user_id) {
     return Image::make(storage_path("app/users/$user_id/cmnd_back.webp"))->response();
 });
 
-Route::get('/test-fcm', function ($user_id) {
+Route::get('/test-fcm/{user_id}', function ($user_id) {
     $res = User::find($user_id);
     if($res->fcm_token) {
         send_fcm($res->fcm_token, 'Đăng nhập thành công test FCM', 'Bạn đã đăng nhập vào hệ thống', 'data');
